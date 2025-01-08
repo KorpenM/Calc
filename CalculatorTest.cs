2683e00a97e079cacd2cf7d1840711badea06eef
Mag Korp <magnuskorp@gmail.com>
2025-01-08 10:32:39 +0100

    added version test

---
+++ CalculatorTest.cs
@@ -0,0 +1,48 @@
+using Xunit;
+
+
+public class CalculatorTest
+{
+
+    [Fact]
+    public void TestAdd()
+    {
+        // given
+        Calculator calc = new();
+
+        int a = 5;
+        int b = 2;
+        int expectedResult = 7;
+
+
+        // when
+        int result = calc.Add(a, b);
+
+        // then
+        Assert.Equal(expectedResult, result);
+
+
+    }
+
+
+    [Fact]
+    public void TestSubtract()
+    {
+        // given
+        Calculator calc = new();
+
+        int a = 5;
+        int b = 2;
+        int expectedResult = 3;
+
+
+        // when
+        int result = calc.Subtract(a, b);
+
+        // then
+        Assert.Equal(expectedResult, result);
+    }
+
+
+
+}
